d73408f069c9ecdc217112e8ee8dfa89928e6016
EmanuelSenaGG <senamanu.ms@gmail.com>
2025-02-16 16:25:37 -0300

    -- inclusão de rotas de aula e salas

---
+++ front/src/rotas/RotasAulas.js
@@ -0,0 +1,4 @@
+const BASEURL = "/aulas";
+const ListarAulas = BASEURL;
+const AlocarAula = BASEURL;
+const ExcluirAula = BASEURL + "/{id}";
---
+++ front/src/rotas/RotasSalas.js
@@ -0,0 +1,5 @@
+const BASEURL = "/salas";
+const ListarSalas = BASEURL;
+const CadastrarSala = BASEURL;
+const EditarSala = BASEURL;
+const ExcluirSala = BASEURL + "/{id}";
