59e003a41150741a7285e7f1c404531e1d4b6858
Andre Lucas <andre12msn@outlook.com>
2025-02-22 13:25:43 -0300

    -- rota para novo endpoint

--- front/src/rotas/RotasAulas.js
+++ front/src/rotas/RotasAulas.js
@@ -1,4 +1,7 @@
 const BASEURL = "/aulas";
 const ListarAulas = BASEURL;
+const ListarAulasPorDisciplinas = BASEURL + "/listarPorDisciplinas";
 const AlocarAula = BASEURL;
 const ExcluirAula = BASEURL + "/{id}";
+
+export {ListarAulas, ListarAulasPorDisciplinas, AlocarAula, ExcluirAula};
